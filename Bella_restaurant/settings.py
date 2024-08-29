6dd0d7cdfdaa3ed47ecd78421668b93a8ab499c7
annabellaals <bebe_chanel_6@hotmail.com>
2024-08-30 02:31:01 +0500

    fixed typo

--- Bella_restaurant/settings.py
+++ Bella_restaurant/settings.py
@@ -100,7 +100,7 @@ PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'yourdatabasename.db'),
+        'NAME': os.path.join(PROJECT_DIR, 'bella_restaurant.db'),
     }
 }
 
